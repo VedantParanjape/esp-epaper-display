6db8fd146a402e4a74014ec19c9e4baff9a32dfa
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-09-18 00:37:31 +0530

    config macros were not being read as sdkconfig.h wasn't included in epaper.h

--- include/epaper.h
+++ include/epaper.h
@@ -1,14 +1,14 @@
 #ifndef EPAPER_H
 #define EPAPER_H
 
+#include "sdkconfig.h"
+
 #ifdef CONFIG_EPD_1_54_V2
 #include "epd1in54_V2.h"
+#endif
 
-#elif CONFIG_EPD_2_13_D
+#ifdef CONFIG_EPD_2_13_D
 #include "epd2in13d.h"
-
-#else 
-#include "epd1in54_V2.h"
 #endif
 
 #include "epdpaint.h"
